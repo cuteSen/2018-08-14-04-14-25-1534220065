e78eb71f7fd34740dd9c8c3e98b0cba8011fd810
wh1621521613 <wh1621521613@163.com>
2019-05-29 14:51:32 +0800

    problem2

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(number) {
+    var line0 = new Array(10);
+    line0 = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.']
+    var line1 = new Array(10)
+    line1 = ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|']
+    var line2 = '|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|'.split('   ')
+    var answer = new Array('','','')
+    for(var i=0;i<number.length;i++)
+    {
+        int_number=parseInt(number[i])
+        answer[0]+=line0[int_number]+(i==number.length-1?'\n':' ')
+        answer[1]+=line1[int_number]+(i==number.length-1?'\n':' ')
+        answer[2]+=line2[int_number]+(i==number.length-1?'\n':' ')
+    }
+    return answer[0]+answer[1]+answer[2];
 };
